aa729a6816bdcce93130fda4b751eeecba37ce1e
ksajjadhossen <sk.sajjad4207@gmail.com>
2025-03-04 17:36:16 +0600

    category swipper added successfully.

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -13,39 +13,36 @@ const Navbar = () => {
 		</>
 	);
 	return (
-		<div className="h-">
-			<div className="navbar fixed z-10 bg-black/30 text-white text-xl shadow-sm ">
-				<div className="navbar max-w-screen-xl ">
-					<div className="dropdown">
-						<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-5 w-5"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								{" "}
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M4 6h16M4 12h8m-8 6h16"
-								/>{" "}
-							</svg>
-						</div>
-						<ul
-							tabIndex={0}
-							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+		<div className="navbar bg-black/30 text-white fixed z-10">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
 						>
-							{navOptions}
-						</ul>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h8m-8 6h16"
+							/>
+						</svg>
 					</div>
-					<a className="btn btn-ghost text-xl">BistroBoss</a>
+					<ul
+						tabIndex={0}
+						className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+					>
+						{navOptions}
+					</ul>
 				</div>
-				<div className="navbar-center hidden lg:flex mx-auto">
-					<ul className="menu menu-horizontal px-1">{navOptions}</ul>
-				</div>
+				<a className="btn btn-ghost text-xl">BistroBoss</a>
+			</div>
+			<div className="navbar-end hidden lg:flex">
+				<ul className="menu menu-horizontal px-1">{navOptions}</ul>
 			</div>
 		</div>
 	);
